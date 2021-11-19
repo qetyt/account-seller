8b5ec7d91545c6aa724f8b7151369beb3e54ae62
qetyt <94569136+qetyt@users.noreply.github.com>
2021-11-19 22:06:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    admins: ["", "", ""], // the admin's ids will set categories and add accounts
-    botID: "", // the probot id
-    transID: "" // id of the one who will take the probot credits
+    admins: ["870698441058164736", "", ""], // the admin's ids will set categories and add accounts
+    botID: ""282859044593598464, // the probot id
+    transID: "870698441058164736" // id of the one who will take the probot credits
 }
